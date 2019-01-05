c43b7d6363f7bdb7620e55731576dab2a7705421
terada <humane-terada>
2019-01-06 08:57:08 +0900

    初期コミット

---
+++ src/jp/co/humane/test/HelloWorld.java
@@ -0,0 +1,17 @@
+package jp.co.humane.test;
+
+/**
+ * gitコマンドテスト用のソース
+ *
+ */
+public class HelloWorld {
+
+    /**
+     * メイン処理。
+     * @param args 引数。
+     */
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+
+}
